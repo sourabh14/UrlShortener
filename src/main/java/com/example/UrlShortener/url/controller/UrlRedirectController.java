120277576026e5d7cc8b06fcf93688f8c77b37d2
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2024-09-16 07:48:39 +0530

    Add dependency for api docs

--- src/main/java/com/example/UrlShortener/url/controller/UrlRedirectController.java
+++ src/main/java/com/example/UrlShortener/url/controller/UrlRedirectController.java
@@ -3,8 +3,6 @@ package com.example.UrlShortener.url.controller;
 import com.example.UrlShortener.url.service.UrlService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
